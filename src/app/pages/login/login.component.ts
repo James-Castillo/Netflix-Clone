a646a35abe1cd15e532c67d5710c93998e2516ae
James Castillo <159741683+James-Castillo@users.noreply.github.com>
2024-04-25 20:11:33 -0600

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone, AfterViewInit } from '@angular/core';
+import { Component, NgZone, AfterViewInit, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -9,48 +9,34 @@ declare var google: any;
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements AfterViewInit {
-
-  constructor(private router: Router, private ngZone: NgZone, private _authService: AuthService) {}
-
-  ngAfterViewInit(): void {
-    const googleBtn = document.getElementById("google-btn");
-    if (!googleBtn) {
-      console.error("Element with ID 'google-btn' not found.");
-      return;
-    }
-
+export class LoginComponent implements OnInit {
+  private router = inject(Router);
+  ngOnInit(): void {
     google.accounts.id.initialize({
       client_id: '29484926712-o2ddub4s3vuefasnhu1cpnqj080g7i11.apps.googleusercontent.com',
-      callback: (resp: any) => this.handleLogin(resp)
+      callback: (resp: any)=> this.handleLogin(resp)
     });
 
-    try {
-      google.accounts.id.renderButton(googleBtn, {
-        theme: 'filled_blue',
-        size: 'large',
-        shape: 'rectangle',
-        width: 350
-      });
-    } catch (error) {
-      console.error("Error rendering Google login button:", error);
-    }
+    google.accounts.id.renderButton(document.getElementById("google-btn"), {
+      theme: 'filled_blue',
+      size: 'large',
+      shape: 'rectangle',
+      width: 350
+    })
   }
 
-  private decodeToken(token: string) {
-    return JSON.parse(atob(token.split(".")[1]))
+  private decodeToken(token: string){
+    return JSON.parse(atob(token.split(".")[1]));
   }
 
-  handleLogin(response: any) {
-    if (response) {
-      // decode the token
-      const payload = this.decodeToken(response.credential);
-      // store in the session
-      sessionStorage.setItem("loggedInUser", JSON.stringify(payload));
-      // navigate to home/browser
-      this.ngZone.run(() => {
-        this.router.navigate(['browse']);
-      });
+  handleLogin(response: any){
+    if(response) {
+      //decode the token
+      const payLoad = this.decodeToken(response.credential);
+      //store in session
+      sessionStorage.setItem("loggedInUser", JSON.stringify(payLoad));
+      //navigate to home/browse
+      this.router.navigate(['browse'])
     }
   }
 }
